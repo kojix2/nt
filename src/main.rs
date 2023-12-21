b6b3689fd5ac4de19aafeea5d7cdfdd07b5f8995
kojix2 <2xijok@gmail.com>
2023-12-21 13:27:17 +0900

    Fix for stdin

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,34 @@
 use notify_rust::Notification;
 use std::env;
-use std::process::Command;
+use std::io::{self, Write};
+use std::process::{exit, Command, Stdio};
 use std::time::Instant;
 
-fn main() -> std::io::Result<()> {
-    // Grab the command-line arguments and concatenate them into a
-    // space-separated string
+fn main() -> io::Result<()> {
+    // Get command line arguments
     let args: Vec<String> = env::args().collect();
     if args.len() < 2 {
-        eprintln!("Usage: {} <command>", args[0]);
-        std::process::exit(1);
+        writeln!(io::stderr(), "Usage: {} <command>", args[0])?;
+        exit(1);
     }
     let command = env::args().skip(1).collect::<Vec<String>>().join(" ");
 
-    // Measure the time of the command execution
+    // Time the command execution
     let start = Instant::now();
-    let output = Command::new("sh").arg("-c").arg(&command).output()?;
+    let mut child = Command::new("sh")
+        .arg("-c")
+        .arg(&command)
+        .stdin(Stdio::inherit())
+        .stdout(Stdio::inherit())
+        .stderr(Stdio::inherit())
+        .spawn()?;
+
+    let output = child.wait()?;
     let elapsed = start.elapsed();
     let elapsed_secs = elapsed.as_secs_f64(); // Convert to float
 
-    // Notify the result of command execution
-    if output.status.success() {
+    // Display notification for the result of command execution
+    if output.success() {
         Notification::new()
             .summary(&format!("Success: {:.2} seconds", elapsed_secs))
             .body(&command)
