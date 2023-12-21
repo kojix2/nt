d425c341b2cd4a0677e61f54b6ae2741dc56bbda
kojix2 <2xijok@gmail.com>
2023-12-21 12:20:18 +0900

    Second commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# nt
+
+'nt' is a tool that runs your command and then sends a desktop notification.
+
+## How to Use
+
+```bash
+nt your command here
+```
+
+This runs your command and then a desktop notification will pop up. The notification tells you if your command worked or didn't work.
+
+## License
+
+'nt' is under the MIT License.
---
+++ src/main.rs
@@ -0,0 +1,32 @@
+use notify_rust::Notification;
+use std::env;
+use std::process::Command;
+use std::time::Instant;
+
+fn main() -> std::io::Result<()> {
+    // Grab the command-line arguments and concatenate them into a
+    // space-separated string
+    let command = env::args().skip(1).collect::<Vec<String>>().join(" ");
+
+    // Measure the time of the command execution
+    let start = Instant::now();
+    let output = Command::new("sh").arg("-c").arg(&command).output()?;
+    let elapsed = start.elapsed();
+    let elapsed_secs = elapsed.as_secs_f64(); // Convert to float
+
+    // Notify the result of command execution
+    if output.status.success() {
+        Notification::new()
+            .summary(&format!("Success: {:.2} seconds", elapsed_secs))
+            .body(&command)
+            .show()
+            .unwrap();
+    } else {
+        Notification::new()
+            .summary(&format!("Failed: {:.2} seconds", elapsed_secs))
+            .body(&command)
+            .show()
+            .unwrap();
+    }
+    Ok(())
+}
